1030c0e6cb568510013a0ef3c6e2d787d99139d6
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-21 10:15:30 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryListRef = document.querySelector('.gallery');
+const renderGallery = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+galleryListRef.insertAdjacentHTML('afterbegin', renderGallery);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
