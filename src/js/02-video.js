aeef7b1ec29ebd5990e620d12de0f60eb6ff7ea9
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-22 21:57:55 +0300

    update 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,12 +10,20 @@ const player = new Player(iframe, {
 });
 
 player.on('timeupdate', throttle(onSaveTimeupdate, 500));
-
+player.on('ended', () => {
+  localStorage.removeItem('videoplayer-current-time');
+});
 getCurrentTimePlay();
 
 function onSaveTimeupdate(event) {
+  console.log('початок');
   const time = event.seconds;
-  localStorage.setItem('videoplayer-current-time', time);
+  console.log(event);
+  if (event.duration - 0.55 < event.seconds) {
+    localStorage.removeItem('videoplayer-current-time');
+  } else {
+    localStorage.setItem('videoplayer-current-time', time);
+  }
 }
 
 function getCurrentTimePlay() {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,60 +2,61 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
-const inputEmailRef = document.querySelector('input[type="email"]');
-const textareaRef = document.querySelector('textarea[name="message"]');
 
 formRef.addEventListener('input', throttle(onFormInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
-// const formData = {
-// };
+const formData = {
+  email: '',
+  message: '',
+};
 
 formStorage();
 
+function clearFormData() {
+  formData.email = '';
+  formData.message = '';
+}
+
 function onFormInput(event) {
-  const formData = {
-    email: formRef.elements.email.value,
-    message: formRef.elements.message.value,
-  };
-
-  // // console.log(formRef.elements.email.value);
-  // console.log(formRef.elements.message.value);
-  // // console.log(inputEmailRef.value);
-  // console.log(textareaRef.value);
-  // console.log(event.target.value);
-
+  formData[event.target.name] = event.target.value;
   // const formData = {
-  //   ...parseSavedFormData,
-  //   [event.target.name]: event.target.value,
+  //   email: formRef.elements.email.value,
+  //   message: formRef.elements.message.value,
   // };
 
-  // formData[event.target.name] = event.target.value;
+  //console.log(formRef.elements.email.value);
+  // console.log(formRef.elements.message.value);
+  // console.log(event.target.value);
 
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
+  const {
+    email: { value: emailValue },
+    message: { value: messageValue },
+  } = event.target.elements;
 
-  if (inputEmailRef.value === '' || textareaRef.value === '') {
-    return console.log('Заповніть всі поля');
+  if (messageValue === '' || emailValue === '') {
+    return alert('Заповніть всі поля');
   }
-  console.log({
-    email: formRef.elements.email.value,
-    message: formRef.elements.message.value,
-  });
+  console.log(formData);
   event.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
+  clearFormData();
 }
 
 function formStorage() {
-  const savedFormData = localStorage.getItem('feedback-form-state');
-  const parseSavedFormData = JSON.parse(savedFormData);
+  const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   if (!savedFormData) {
     return;
   }
-  formRef.elements.email.value = parseSavedFormData.email;
-  formRef.elements.message.value = parseSavedFormData.message;
+  formData.email = savedFormData.email;
+  formData.message = savedFormData.message;
+
+  formRef.elements.email.value = savedFormData.email;
+  formRef.elements.message.value = savedFormData.message;
 }
